4821666ad2e145b2ea4138dbd2935df717e44bfb
huyHoangUran <huyhoangnguyen.work@gmail.com>
2022-09-27 00:42:32 +0700

    hoang lesson_1

---
+++ lesson_1/ex01/main.js
@@ -0,0 +1,8 @@
+// // var str = getElementById(str);
+// function onClick(){
+//     // console.log('button')
+//     var str = document.getElementById('str').value;
+//     var number =document.getElementById('number').value;
+//     if(str.length)
+//     // console.log(str.slice(0,number)+'...');
+// }
